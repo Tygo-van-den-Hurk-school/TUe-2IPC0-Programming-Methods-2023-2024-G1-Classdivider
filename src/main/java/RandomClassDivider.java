7febc4b6f8008a9048c4229b5331daecff9745a5
Tygo van den Hurk <hazels.05.remount@icloud.com>
2023-12-12 10:44:58 +0100

    added RandomClassDivider as subClass of ClassDivider

---
+++ src/main/java/RandomClassDivider.java
@@ -0,0 +1,13 @@
+/**
+ * Divides {@code Student}s into {@code Group}s randomly.
+ * 
+ * @author Tygo van den Hurk
+ * @version 1.0
+ * 
+ * @see ClassDivider
+ * @see Student
+ * @see Group
+ */
+public class RandomClassDivider extends ClassDivider {
+    
+}
